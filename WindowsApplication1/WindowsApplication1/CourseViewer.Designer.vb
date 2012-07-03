ae106170c56a656d4cb59550385612ebba0ae43d
Andrew Scholer <ascholer@ascholer.org>
2012-07-03 09:11:24 -0700

    Initial upload

---
+++ WindowsApplication1/WindowsApplication1/CourseViewer.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CourseViewer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cboDeptList = New System.Windows.Forms.ComboBox()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.dgvCourse = New System.Windows.Forms.DataGridView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.dgvCourse, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cboDeptList
+        '
+        Me.cboDeptList.FormattingEnabled = True
+        Me.cboDeptList.Location = New System.Drawing.Point(24, 29)
+        Me.cboDeptList.Name = "cboDeptList"
+        Me.cboDeptList.Size = New System.Drawing.Size(121, 21)
+        Me.cboDeptList.TabIndex = 0
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(197, 27)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(116, 23)
+        Me.btnClose.TabIndex = 1
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'dgvCourse
+        '
+        Me.dgvCourse.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvCourse.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvCourse.Location = New System.Drawing.Point(2, 109)
+        Me.dgvCourse.Name = "dgvCourse"
+        Me.dgvCourse.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvCourse.Size = New System.Drawing.Size(380, 150)
+        Me.dgvCourse.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 276)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(376, 201)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(197, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(116, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Save Changes"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CourseViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(385, 489)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.dgvCourse)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.cboDeptList)
+        Me.Name = "CourseViewer"
+        Me.Text = "Form1"
+        CType(Me.dgvCourse, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cboDeptList As System.Windows.Forms.ComboBox
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents dgvCourse As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/CourseViewer.vb
@@ -0,0 +1,92 @@
+﻿Imports System.Data.Objects
+Imports System.Data.Objects.DataClasses
+
+Public Class CourseViewer
+
+    Private schoolContext As SchoolEntities
+
+
+    Private Sub btnClose_Click(sender As System.Object, e As System.EventArgs) Handles btnClose.Click
+        schoolContext.Dispose()
+        Me.Close()
+    End Sub
+
+    Private Sub CourseViewer_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        ' Initialize the ObjectContext.
+        schoolContext = New SchoolEntities()
+
+
+
+        ' Define a query that returns all Department objects and related
+        ' Course objects, ordered by name.
+        Dim departmentQuery As ObjectQuery(Of Department) = _
+            schoolContext.Departments.OrderBy("it.Name")
+
+        Try
+            ' Bind the ComboBox control to the query, which is 
+            ' executed during data binding.
+            Me.cboDeptList.DisplayMember = "Name"
+            Me.cboDeptList.DataSource = departmentQuery
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub cboDeptList_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cboDeptList.SelectedIndexChanged
+        Try
+            ' Get the object for the selected department.
+            Dim department As Department = _
+                CType(Me.cboDeptList.SelectedItem, Department)
+
+            ' Bind the grid view to the collection of Course objects 
+            ' that are related to the selected Department object.
+            dgvCourse.DataSource = department.Courses
+
+            ' Hide the columns that are bound to the navigation properties on Course.
+            dgvCourse.Columns("Department").Visible = False
+            dgvCourse.Columns("CourseGrades").Visible = False
+            dgvCourse.Columns("OnlineCourse").Visible = False
+            dgvCourse.Columns("OnsiteCourse").Visible = False
+            dgvCourse.Columns("People").Visible = False
+
+            dgvCourse.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+
+
+    Private Sub dgvCourse_SelectionChanged(sender As System.Object, e As System.EventArgs) Handles dgvCourse.SelectionChanged
+        Dim c As Course = dgvCourse.CurrentRow.DataBoundItem
+        TextBox1.Text = ""
+        If c IsNot Nothing Then
+            For Each cg As CourseGrade In c.CourseGrades
+                TextBox1.Text &= cg.Person.FirstName & cg.Grade & vbNewLine
+            Next
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Try
+            ' Save object changes to the database, display a message, 
+            ' and refresh the form.
+            schoolContext.SaveChanges()
+            MessageBox.Show("Changes saved to the database.")
+            Me.Refresh()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+
+
+    Private Sub dgvCourse_UserDeletingRow(sender As System.Object, e As System.Windows.Forms.DataGridViewRowCancelEventArgs) Handles dgvCourse.UserDeletingRow
+        schoolContext.DeleteObject(dgvCourse.CurrentRow.DataBoundItem)
+        e.Cancel = True
+    End Sub
+End Class
